85dd57a5d5ae32eaaa4f063904b6cb9c62bf0685
Marius Olafsen <mariubolafs@gmail.com>
2022-10-03 10:17:03 +0200

    basic init

---
+++ src/test/java/no/noroff/lagalt/LagaltApplicationTests.java
@@ -0,0 +1,13 @@
+package no.noroff.lagalt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LagaltApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
