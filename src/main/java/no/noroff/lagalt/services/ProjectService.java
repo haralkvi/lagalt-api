a11cfe7414f2016e8bcc68d4313ca903516f562d
Harald Kvisli <haraldkv@runbox.com>
2022-10-04 09:51:24 +0200

    feat: implemented basic controller for User model; declared class UserService

--- src/main/java/no/noroff/lagalt/services/ProjectService.java
+++ src/main/java/no/noroff/lagalt/services/ProjectService.java
@@ -6,12 +6,15 @@ import java.util.Collection;
 
 public class ProjectService {
     public Collection<Project> findAll() {
+        return null;
     }
 
     public Project findById(int id) {
+        return null;
     }
 
     public Project add(Project project) {
+        return null;
     }
 
     public void update(Project project) {
