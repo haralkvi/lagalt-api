65ec242918bffcd5673f9f9b6318c3950e008b58
Harald Kvisli <haraldkv@runbox.com>
2022-10-04 09:39:30 +0200

    feat: added basic endpoints inside project controller

--- src/main/java/no/noroff/lagalt/repositories/CommentRepository.java
+++ src/main/java/no/noroff/lagalt/repositories/CommentRepository.java
@@ -1,5 +1,6 @@
 package no.noroff.lagalt.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import no.noroff.lagalt.models.Comment;
 
--- src/main/java/no/noroff/lagalt/repositories/ProjectRepository.java
+++ src/main/java/no/noroff/lagalt/repositories/ProjectRepository.java
@@ -1,6 +1,7 @@
 package no.noroff.lagalt.repositories;
 
 import no.noroff.lagalt.models.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProjectRepository extends JpaRepository<Project, Integer> {
 }
