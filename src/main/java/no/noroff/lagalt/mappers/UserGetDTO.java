3937caf52c16079bead1f971879a901580501c47
Marius Olafsen <covesthur@gmail.com>
2022-10-06 14:35:03 +0200

    usegetdto hotfix

--- src/main/java/no/noroff/lagalt/mappers/UserGetDTO.java
+++ src/main/java/no/noroff/lagalt/mappers/UserGetDTO.java
@@ -7,12 +7,15 @@ import java.util.Set;
 @Data
 public class UserGetDTO {
 
+    private int id;
     private String name;
     private String email;
     private String description;
     //private String skillSet;
     private Set<Integer> projectsOwned;
-
+    //legg til disse hvis nødvendig
+    //private Set<Integer> projectsHistory;
+    //private Set<Integer> projetsParticipated;
     //skal disse være med?
     private boolean hidden;
     private boolean admin;
