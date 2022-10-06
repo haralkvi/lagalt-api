064d589c697460911f57925de71beb5a096fcdc7
Marius Olafsen <covesthur@gmail.com>
2022-10-06 14:35:30 +0200

    project DTO + basic mappers

---
+++ src/main/java/no/noroff/lagalt/mappers/ProjectMapper.java
@@ -0,0 +1,34 @@
+package no.noroff.lagalt.mappers;
+
+import no.noroff.lagalt.models.Project;
+import no.noroff.lagalt.models.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Mapper(componentModel = "spring")
+public abstract class ProjectMapper {
+
+    @Mapping(target = "owner", source = "owner", qualifiedByName = "ownerToString")
+    public abstract ProjectGetDTO projectToProjectDTO(Project project);
+
+    public Collection<ProjectGetDTO> projectToProjectDTO(Collection<Project> projects) {
+        if (projects == null) {
+            return null;
+        }
+        Collection<ProjectGetDTO> list = new ArrayList<ProjectGetDTO>(projects.size());
+        for (Project project : projects) {
+            list.add(projectToProjectDTO(project));
+        }
+        return list;
+    }
+
+    @Named("ownerToString")
+    String mapToString(User value){
+        return value.getName();
+    }
+
+}
