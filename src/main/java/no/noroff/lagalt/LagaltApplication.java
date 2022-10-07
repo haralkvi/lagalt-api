dd4f6aa8872cbe306705cec1746e59800c447132
Harald Kvisli <haraldkv@runbox.com>
2022-10-07 08:59:43 +0200

    feat: added request mappign for root

--- src/main/java/no/noroff/lagalt/LagaltApplication.java
+++ src/main/java/no/noroff/lagalt/LagaltApplication.java
@@ -2,13 +2,22 @@ package no.noroff.lagalt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class LagaltApplication {
 
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
     public static void main(String[] args) {
 
-        System.out.println(System.getenv("DATASOURCE_URL"));
         SpringApplication.run(LagaltApplication.class, args);
     }
 
