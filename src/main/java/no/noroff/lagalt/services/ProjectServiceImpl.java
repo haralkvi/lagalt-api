e3c056f70aead018c5e2c312b4596a1a20dd6dca
haralkvi <harald@runbox.com>
2022-10-12 14:05:35 +0200

    refactor(ProjectServiceImpl): removed redundant lines.

--- src/main/java/no/noroff/lagalt/services/ProjectServiceImpl.java
+++ src/main/java/no/noroff/lagalt/services/ProjectServiceImpl.java
@@ -51,20 +51,14 @@ public class ProjectServiceImpl implements ProjectService {
         if (projectRepository.existsById(id)) {
             Project project = this.findById(id);
 
-            User projectOwner = project.getOwner();
-            Collection<User> projectMembers = project.getMembers();
-            Collection<User> projectViewers = project.getUserViews();
-            Collection<Application> projectApplications = project.getApplications();
-            Collection<Comment> projectComments = project.getComments();
-
             // Users who have owned, been member of or viewed a project are not deleted when project is deleted
-            projectOwner.getProjectsOwned().remove(project);
-            projectMembers.forEach(member -> member.getProjectsParticipated().remove(project));
-            projectViewers.forEach(viewer -> viewer.getProjectsHistory().remove(project));
+            project.getOwner().getProjectsOwned().remove(project);
+            project.getMembers().forEach(member -> member.getProjectsParticipated().remove(project));
+            project.getUserViews().forEach(viewer -> viewer.getProjectsHistory().remove(project));
 
             // Applications and comments belonging to a project are deleted when project is deleted
-            projectApplications.forEach(application -> applicationService.delete(application));
-            projectComments.forEach(comment -> commentService.delete(comment));
+            project.getApplications().forEach(application -> applicationService.delete(application));
+            project.getComments().forEach(comment -> commentService.delete(comment));
 
             // project can safely be deleted
             projectRepository.deleteById(id);
