b1940c33be3cc18bcaf3853a0deebd8c08dc537a
haralkvi <harald@runbox.com>
2022-10-13 16:43:29 +0200

    fix: removed redundat print statement

--- src/main/java/no/noroff/lagalt/controllers/ProjectController.java
+++ src/main/java/no/noroff/lagalt/controllers/ProjectController.java
@@ -38,7 +38,6 @@ public class ProjectController {
     })
     @GetMapping
     public ResponseEntity<?> getAll() {
-        System.out.println("DEBUG: ");
         Collection<ProjectGetDTO> projects = projectMapper.projectToProjectDTO(projectService.findAll());
         if (projects.size()>0){
             return new ResponseEntity<>(projects, HttpStatus.OK);
