0acf76091398b676224e555134992e5705f089a5
haralkvi <harald@runbox.com>
2022-10-17 11:40:17 +0200

    fix(CommentPostDTO): changed type of user id from int to string.

--- src/main/java/no/noroff/lagalt/dtos/CommentPostDTO.java
+++ src/main/java/no/noroff/lagalt/dtos/CommentPostDTO.java
@@ -7,7 +7,7 @@ import lombok.Setter;
 @Data
 public class CommentPostDTO {
     private int project;
-    private int user;
+    private String user;
     private String text;
     private String time;
 }
