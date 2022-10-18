807944c15a9c4b1404f558069343a92dc1c123d5
haralkvi <harald@runbox.com>
2022-10-18 08:09:28 +0200

    refactor(CommentGetDTO): user and project changed to types UserDetails and ProjectDetails respectively.

--- src/main/java/no/noroff/lagalt/dtos/get/CommentGetDTO.java
+++ src/main/java/no/noroff/lagalt/dtos/get/CommentGetDTO.java
@@ -1,13 +1,15 @@
 package no.noroff.lagalt.dtos.get;
 
 import lombok.Data;
+import no.noroff.lagalt.dtos.details.ProjectDetails;
+import no.noroff.lagalt.dtos.details.UserDetails;
 
 @Data
 public class CommentGetDTO {
 
     private int id;
-    private int project;
-    private String user;
+    private ProjectDetails project;
+    private UserDetails user;
     private String text;
     private String time;
 
