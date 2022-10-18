364ba102017fd163f0e9150a4eeccbb7fd819fbf
haralkvi <harald@runbox.com>
2022-10-18 08:28:24 +0200

    fix(UserMapper): made changes to mapper to reflect changes in dto.

--- src/main/java/no/noroff/lagalt/mappers/UserMapper.java
+++ src/main/java/no/noroff/lagalt/mappers/UserMapper.java
@@ -1,11 +1,14 @@
 package no.noroff.lagalt.mappers;
 
+import no.noroff.lagalt.dtos.details.ProjectDetails;
+import no.noroff.lagalt.dtos.details.UserDetails;
 import no.noroff.lagalt.dtos.get.UserGetDTO;
 import no.noroff.lagalt.dtos.post.UserPostDTO;
 import no.noroff.lagalt.models.Project;
 import no.noroff.lagalt.models.User;
 import no.noroff.lagalt.models.UserPutDTO;
 import org.mapstruct.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -14,9 +17,14 @@ import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public abstract class UserMapper {
-    @Mapping(target = "projectsParticipated", source = "projectsParticipated", qualifiedByName = "projectsToIds")
-    @Mapping(target = "projectsHistory", source = "projectsHistory", qualifiedByName = "projectsToIds")
-    @Mapping(target = "projectsOwned", source = "projectsOwned", qualifiedByName = "projectsToIds")
+
+    @Autowired
+    ProjectDetailsMapper projectDetailsMapper;
+
+
+    @Mapping(target = "projectsParticipated", source = "projectsParticipated", qualifiedByName = "projectToProjectDetails")
+    @Mapping(target = "projectsHistory", source = "projectsHistory", qualifiedByName = "projectToProjectDetails")
+    @Mapping(target = "projectsOwned", source = "projectsOwned", qualifiedByName = "projectToProjectDetails")
     public abstract UserGetDTO userToUserDTO(User user);
 
     @Mapping(target = "hidden", constant = "false")
@@ -35,6 +43,20 @@ public abstract class UserMapper {
         }
         return list;
     }
+
+    @Named("projectToProjectDetails")
+    ProjectDetails mapProjectToDetails(Project project) {
+        return projectDetailsMapper.projectToProjectDetails(project);
+    }
+
+    @Named("projectsToProjectDetails")
+    Set<ProjectDetails> mapProjectsToDetails(Set<Project> projects) {
+        if (projects == null)
+            return null;
+        return projects.stream()
+                .map(this::mapProjectToDetails).collect(Collectors.toSet());
+    }
+
 
     @Named("projectsToIds")
     Set<Integer> mapToInteger(Set<Project> projects) {
