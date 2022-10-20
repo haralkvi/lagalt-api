b9d67ee7bd8b61f822cacc09f16a97455fa7238e
Marius Olafsen <covesthur@gmail.com>
2022-10-20 08:57:47 +0200

    commentcontrollertest finished

--- src/test/java/no/noroff/lagalt/controller/CommentControllerTest.java
+++ src/test/java/no/noroff/lagalt/controller/CommentControllerTest.java
@@ -1,8 +1,13 @@
 package no.noroff.lagalt.controller;
 
 import no.noroff.lagalt.controllers.CommentController;
+import no.noroff.lagalt.dtos.get.CommentGetDTO;
+import no.noroff.lagalt.dtos.post.CommentPostDTO;
 import no.noroff.lagalt.mappers.CommentMapper;
+import no.noroff.lagalt.models.Comment;
 import no.noroff.lagalt.services.CommentService;
+import no.noroff.lagalt.services.ProjectService;
+import no.noroff.lagalt.services.UserService;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -12,9 +17,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 
@@ -31,34 +38,141 @@ public class CommentControllerTest {
     @Mock
     private CommentMapper commentMapper;
 
+    @Mock
+    private UserService userService;
+
+    @Mock
+    private ProjectService projectService;
+
 
     //get all
+    @Test
+    public void TestGetAll_ReturnOK(){
+        //arrange
+        Comment comment = new Comment();
+        Collection<Comment> comments = new ArrayList<>();
+        comments.add(comment);
+        Collection<CommentGetDTO> dtos = new ArrayList<>();
 
+        when(commentMapper.commentToCommentDTO(anyCollection())).thenReturn(dtos);
+        when(commentService.findAll()).thenReturn(comments);
+        //act
+        ResponseEntity<?> result = commentController.getAll();
+        //assert
+        assertEquals(HttpStatus.OK, result.getStatusCode());
+    }
     //get all not found
+    @Test
+    public void TestAll_ReturnNotFound(){
+        //arrange
+        Collection<Comment> comments = new ArrayList<>();
+        Collection<CommentGetDTO> dtos = new ArrayList<>();
+
+        when(commentMapper.commentToCommentDTO(anyCollection())).thenReturn(dtos);
+        when(commentService.findAll()).thenReturn(comments);
+        //act
+        ResponseEntity<?> result = commentController.getAll();
+        //assert
+        assertEquals(HttpStatus.NOT_FOUND, result.getStatusCode());
+    }
 
     //getById
+    @Test
+    public void TestGetById_ReturnOK(){
+        //arrange
+        CommentGetDTO commentGetDTO = new CommentGetDTO();
+        Comment comment = new Comment();
 
+        when(commentMapper.commentToCommentDTO(any(Comment.class))).thenReturn(commentGetDTO);
+        when(commentService.findById(anyInt())).thenReturn(comment);
+        //act
+        ResponseEntity<?> result = commentController.getById(1);
+        //assert
+        assertEquals(HttpStatus.OK, result.getStatusCode());
+    }
     //getById not found
+    @Test
+    public void TestGetById_ReturnNotFound(){
+        //arrange
+        Comment comment = new Comment();
+
+        when(commentMapper.commentToCommentDTO(any(Comment.class))).thenReturn(null);
+        when(commentService.findById(anyInt())).thenReturn(comment);
+        //act
+        ResponseEntity<?> result = commentController.getById(5);
+        //assert
+        assertEquals(HttpStatus.NOT_FOUND, result.getStatusCode());
+    }
+
 
     //add
+    @Test
+    public void TestAdd_ReturnCreated(){
+        //arrange
+        CommentPostDTO commentPostDTO = new CommentPostDTO();
+        Comment comment = new Comment();
+
+        when(userService.existsById(anyString())).thenReturn(true);
+        when(projectService.existsById(anyInt())).thenReturn(true);
+        when(commentMapper.commentPostDTOtoComment(any(CommentPostDTO.class))).thenReturn(comment);
+        when(commentService.add(any(Comment.class))).thenReturn(comment);
+        //act
+        ResponseEntity<?> result = commentController.add(commentPostDTO);
+        //assert
+        assertEquals(HttpStatus.CREATED, result.getStatusCode());
+    }
 
     //add bad request
+    @Test
+    public void TestAdd_ReturnBadRequest(){
+        //arrange
+        CommentPostDTO commentPostDTO = new CommentPostDTO();
+        Comment comment = new Comment();
+
+        when(userService.existsById(anyString())).thenReturn(false);
+        when(projectService.existsById(anyInt())).thenReturn(false);
+        when(commentMapper.commentPostDTOtoComment(any(CommentPostDTO.class))).thenReturn(comment);
+        when(commentService.add(any(Comment.class))).thenReturn(comment);
+        //act
+        ResponseEntity<?> result = commentController.add(commentPostDTO);
+        //assert
+        assertEquals(HttpStatus.NO_CONTENT, result.getStatusCode());
+    }
 
     //delete
+    @Test
+    public void TestDelete_ReturnNoContent(){
+        //arrange
+        doNothing().when(commentService).deleteById(anyInt());
+        //act
+        ResponseEntity<?> result = commentController.delete(5);
+        //assert
+        assertEquals(HttpStatus.NO_CONTENT, result.getStatusCode());
+    }
+    //delete bad request TODO: why not 404
+    @Test
+    public void TestDelete_ReturnBadRequest(){
+        //arrange
+        doNothing().when(commentService).deleteById(anyInt());
+        //act
+        ResponseEntity<?> result = commentController.delete(0);
 
-    //delete bad request why not 404
+        //assert
+        assertEquals(HttpStatus.NOT_FOUND, result.getStatusCode());
+    }
+
 
     //editcomment
     @Test
     public void TestEdit_ReturnNoContent(){
         //arrange
-        when(commentService.existsById(anyInt())).thenReturn(null);
+        when(commentService.existsById(anyInt())).thenReturn(true);
         doNothing().when(commentService).updateText(anyString(),anyInt());
         //act
         ResponseEntity<?> result = commentController.editComment("Test",5);
 
         //assert
-        assertEquals(HttpStatus.NOT_FOUND, result.getStatusCode());
+        assertEquals(HttpStatus.NO_CONTENT, result.getStatusCode());
     }
 
 
@@ -66,7 +180,7 @@ public class CommentControllerTest {
     @Test
     public void TestEdit_ReturnNotFound(){
         //arrange
-        when(commentService.existsById(anyInt())).thenReturn(null);
+        when(commentService.existsById(anyInt())).thenReturn(false);
         doNothing().when(commentService).updateText(anyString(),anyInt());
         //act
         ResponseEntity<?> result = commentController.editComment("Test",5);
@@ -74,6 +188,4 @@ public class CommentControllerTest {
         //assert
         assertEquals(HttpStatus.NOT_FOUND, result.getStatusCode());
     }
-
-
 }
